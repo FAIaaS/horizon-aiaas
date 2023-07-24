3b1ce211dd294f554edbf6dc69b08e9e3e3d952b
Oleg Sadov <oleg.sadov@gmail.com>
2023-07-24 11:43:29 +0300

    Rmoved 'build' directory
    Signed-off-by: Oleg Sadov <oleg.sadov@gmail.com>

--- build/lib/openstack_dashboard/enabled/_1520_project_floating_ip_portforwardings_panel.py
+++
@@ -1,7 +0,0 @@
-PANEL_DASHBOARD = 'project'
-PANEL_GROUP = 'network'
-PANEL = 'floating_ip_portforwardings'
-
-ADD_PANEL = \
-    'openstack_dashboard.dashboards.project.floating_ip_portforwardings.panel' \
-    '.FloatingIpPortforwardingRules'
